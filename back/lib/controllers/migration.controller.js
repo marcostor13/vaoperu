19001153c41b70a3bd102452df259824c904bbc8
Marcos Torres <marcostor13@gmail.com>
2022-12-04 06:40:17 -0500

    update migration

--- back/lib/controllers/migration.controller.js
+++ back/lib/controllers/migration.controller.js
@@ -11,7 +11,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.execute = exports.init = exports.getProvider = void 0;
 const mysql_1 = require("mysql");
+const district_1 = require("../models/district");
+const profile_provider_1 = require("../models/profile-provider");
 const user_1 = require("../models/user");
+const url_1 = require("./../models/url");
+const category_subcategory_profile_1 = require("./../models/category-subcategory-profile");
+const item_section_1 = require("../models/item-section");
+const subitem_section_1 = require("../models/subitem-section");
+const product_1 = require("../models/product");
 const pool = () => {
     return new Promise((resolve, reject) => {
         const con = mysql_1.createPool({
@@ -47,10 +54,37 @@ exports.init = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const id = req.body.id;
     try {
         const query = `
-            SELECT users.name as name, users.email, users.password, role_user.role_id FROM users 
+            SELECT DISTINCT 
+            users.name as name, 
+            users.email, 
+            users.password, 
+            role_user.role_id,
+            companies.id as companyid, 
+            companies.legalname,
+            companies.comercialname,
+            companies.phone1,
+            companies.phone2,
+            companies.address1,
+            companies.address2,
+            companies.ruc, 
+            companies.description,
+            companies.image,
+            companies.facebook, 
+            companies.twitter,
+            companies.instagram,
+            companies.google,
+            companies.siteweb,
+            companies.district,
+            companies.lat, 
+            companies.lng,
+            companies.video,
+            companies.enabled,
+            urls.url
+            FROM users 
             INNER JOIN role_user ON role_user.user_id=users.id 
-            INNER JOIN urls
-            WHERE users.id=${id}`;
+            INNER JOIN companies ON companies.userid=users.id
+            INNER JOIN urls ON urls.companyId=companies.id
+            WHERE users.id=${id} AND role_user.role_id=2`;
         const userData = (yield exports.execute(query));
         if (userData) {
             const resp = userData[0];
@@ -76,9 +110,141 @@ exports.init = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             }
             const newUser = new user_1.default(body);
             const newU = yield newUser.save();
-            return res.status(500).json({
+            //SAVE PROFILE PROVIDER
+            const districts = yield district_1.default.find({});
+            const district = districts.find(d => d.name.toLowerCase().trim() === resp.district.toLowerCase().trim());
+            let districtId = '';
+            let districtName = '';
+            if (district) {
+                districtId = district._id;
+                districtName = district.name;
+            }
+            const profileProvider = {
+                userid: newU._id,
+                legalName: resp.legalname,
+                comercialName: resp.comercialname,
+                whatsapp: resp.phone1,
+                phone: resp.phone1,
+                addressText: resp.adrress2,
+                addressMaps: resp.adrress1,
+                ruc: resp.ruc,
+                description: resp.description,
+                image: resp.image,
+                video: resp.video,
+                facebook: resp.facebook,
+                twitter: resp.twitter,
+                instagram: resp.instagram,
+                tiktok: resp.tiktok,
+                email: newU.email,
+                districtId,
+                distrinctName: districtName,
+                lat: resp.lat,
+                lng: resp.lng,
+                siteWeb: resp.siteWeb,
+                delivery: false,
+                onMarket: true,
+                enabledProducts: true,
+                enabledProductsFeatured: true,
+                enabledOffers: true,
+                openTime: '08:00',
+                closeTime: '23:59',
+                role: ''
+            };
+            const profile = new profile_provider_1.default(profileProvider);
+            const newProfile = yield profile.save();
+            //SAVE URL
+            const urlData = {
+                url: resp.url.toLowerCase().trim(),
+                profileProviderId: newProfile._id,
+                isIndividual: false
+            };
+            const url = new url_1.default(urlData);
+            const newUrl = yield url.save();
+            //SAVE CATEGORIES SUBCATEGORIES PROFILE
+            const items = yield item_section_1.default.find({});
+            const subitems = yield subitem_section_1.default.find({});
+            const query2 = `
+                SELECT distinct
+                categories.name as categoryName,
+                companyid
+                FROM category_companies
+                INNER JOIN categories ON categories.id = category_companies.categoryid
+                WHERE companyid = ${resp.companyid} `;
+            const categoriesCompanyData = (yield exports.execute(query2));
+            const query3 = `
+                SELECT distinct
+                subcategories.name as subcategoryName,
+                companyid
+                FROM category_companies
+                INNER JOIN subcategories ON subcategories.id = category_companies.subcategoryid
+                WHERE companyid = ${resp.companyid} `;
+            const subcategoriesCompanyData = (yield exports.execute(query3));
+            categoriesCompanyData.forEach((c) => __awaiter(void 0, void 0, void 0, function* () {
+                const itemSearch = items.find(item => item.name.toLowerCase().trim() === c.categoryName.toLowerCase().trim());
+                if (itemSearch) {
+                    const CSPData = {
+                        name: itemSearch.name,
+                        categorySubcategoryId: itemSearch._id,
+                        type: 'item',
+                        profileProviderId: newProfile._id
+                    };
+                    const cSProfile = new category_subcategory_profile_1.default(CSPData);
+                    yield cSProfile.save();
+                }
+            }));
+            subcategoriesCompanyData.forEach((s) => __awaiter(void 0, void 0, void 0, function* () {
+                const subitemSearch = subitems.find(subitem => subitem.name.toLowerCase().trim() === s.subcategoryName.toLowerCase().trim());
+                if (subitemSearch) {
+                    const CSPData = {
+                        name: subitemSearch.name,
+                        categorySubcategoryId: subitemSearch._id,
+                        type: 'subitem',
+                        profileProviderId: newProfile._id
+                    };
+                    const cSProfile = new category_subcategory_profile_1.default(CSPData);
+                    yield cSProfile.save();
+                }
+            }));
+            //SAVE PRODUCTS
+            console.log('resp.companyid', resp.companyid);
+            const query4 = `
+                SELECT 
+                id,
+                name,
+                description,
+                featured,
+                promotionalprice,
+                price
+                FROM services
+                WHERE companyid = ${resp.companyid} `;
+            const servicesData = (yield exports.execute(query4));
+            servicesData.forEach((s) => __awaiter(void 0, void 0, void 0, function* () {
+                //QUERY IMAGES
+                const query5 = `
+                    SELECT *
+                    FROM product_images
+                    WHERE productid = ${s.id} `;
+                const productImagesData = (yield exports.execute(query5));
+                let images = [];
+                if (productImagesData) {
+                    images = productImagesData.map((p) => { return p.image; });
+                }
+                const product = {
+                    name: s.name,
+                    profileProviderId: newProfile._id,
+                    description: s.description,
+                    images: images,
+                    price: parseInt(s.price) || null,
+                    isFeatured: s.featured,
+                    promotionalPrice: parseInt(s.promocionalprice) || null,
+                    categoryId: '',
+                };
+                const prod = new product_1.default(product);
+                yield prod.save();
+            }));
+            return res.status(200).json({
                 message: `Migración completada`,
-                data: newU
+                data: newUrl
             });
         }
         else {
--- back/src/controllers/migration.controller.ts
+++ back/src/controllers/migration.controller.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express";
 import { createPool } from 'mysql';
+import District, { IDistrict } from "../models/district";
+import ProfileProvider, { IProfileProvider } from "../models/profile-provider";
 import User, { Iuser } from "../models/user";
+import Url, { IUrl } from './../models/url';
+import CategorySubcategoryProfile, { ICategorySubcategoryProfile } from './../models/category-subcategory-profile';
+import ItemSection from "../models/item-section";
+import SubitemSection from "../models/subitem-section";
+import Product, { IProduct } from "../models/product";
 
 const pool =  () => {
     return new Promise((resolve, reject) => {
@@ -40,10 +47,37 @@ export const init = async (req: Request, res: Response) => {
     
     try {
         const query = `
-            SELECT users.name as name, users.email, users.password, role_user.role_id FROM users 
+            SELECT DISTINCT 
+            users.name as name, 
+            users.email, 
+            users.password, 
+            role_user.role_id,
+            companies.id as companyid, 
+            companies.legalname,
+            companies.comercialname,
+            companies.phone1,
+            companies.phone2,
+            companies.address1,
+            companies.address2,
+            companies.ruc, 
+            companies.description,
+            companies.image,
+            companies.facebook, 
+            companies.twitter,
+            companies.instagram,
+            companies.google,
+            companies.siteweb,
+            companies.district,
+            companies.lat, 
+            companies.lng,
+            companies.video,
+            companies.enabled,
+            urls.url
+            FROM users 
             INNER JOIN role_user ON role_user.user_id=users.id 
-            INNER JOIN urls
-            WHERE users.id=${id}`
+            INNER JOIN companies ON companies.userid=users.id
+            INNER JOIN urls ON urls.companyId=companies.id
+            WHERE users.id=${id} AND role_user.role_id=2`
         const userData:any = (await execute(query))
 
         if(userData){
@@ -65,16 +99,176 @@ export const init = async (req: Request, res: Response) => {
             }
             const user = await User.findOne({email: email})
             if(user){
-                 return res.status(400).json({
+                return res.status(400).json({
                     message: `El usuario ya existe`
                 }) 
             }
             const newUser:Iuser = new User(body)
             const newU: any = await newUser.save()
-            return res.status(500).json({
-                message: `Migración completada`,
-                data:newU
+
+            //SAVE PROFILE PROVIDER
+
+            const districts: IDistrict[] = await District.find({})
+            const district= districts.find(d=> d.name.toLowerCase().trim() === resp.district.toLowerCase().trim())
+            let districtId = ''
+            let districtName = ''
+            if(district){
+                districtId = district._id
+                districtName = district.name
+            }
+
+            const profileProvider = {
+                userid: newU._id,
+                legalName: resp.legalname,
+                comercialName: resp.comercialname,
+                whatsapp: resp.phone1,
+                phone: resp.phone1,
+                addressText: resp.adrress2,
+                addressMaps: resp.adrress1,
+                ruc: resp.ruc,
+                description: resp.description,
+                image: resp.image,
+                video: resp.video,
+                facebook: resp .facebook,
+                twitter: resp.twitter,
+                instagram: resp.instagram,
+                tiktok: resp.tiktok,
+                email: newU.email,
+                districtId,
+                distrinctName: districtName,
+                lat: resp.lat,                 
+                lng: resp.lng,
+                siteWeb: resp.siteWeb,
+                delivery: false,
+                onMarket: true,
+                enabledProducts: true,
+                enabledProductsFeatured: true,
+                enabledOffers: true,
+                openTime: '08:00',
+                closeTime: '23:59',
+                role: ''
+            }
+
+            const profile: IProfileProvider = new ProfileProvider(profileProvider)
+            const newProfile = await profile.save()
+
+            //SAVE URL
+
+            const urlData = {
+                url: resp.url.toLowerCase().trim(),
+                profileProviderId:newProfile._id,
+                isIndividual: false
+            }
+
+            const url: IUrl = new Url(urlData)
+            const newUrl = await url.save()
+
+            //SAVE CATEGORIES SUBCATEGORIES PROFILE
+
+            const items = await ItemSection.find({})
+            const subitems = await SubitemSection.find({})
+
+            const query2 = `
+                SELECT distinct
+                categories.name as categoryName,
+                companyid
+                FROM category_companies
+                INNER JOIN categories ON categories.id = category_companies.categoryid
+                WHERE companyid = ${resp.companyid} `
+            const categoriesCompanyData:any = (await execute(query2))
+
+            const query3 = `
+                SELECT distinct
+                subcategories.name as subcategoryName,
+                companyid
+                FROM category_companies
+                INNER JOIN subcategories ON subcategories.id = category_companies.subcategoryid
+                WHERE companyid = ${resp.companyid} `
+            const subcategoriesCompanyData:any = (await execute(query3))   
+            
+
+            categoriesCompanyData.forEach(async (c:any)=>{
+                const itemSearch = items.find(item=>item.name.toLowerCase().trim() === c.categoryName.toLowerCase().trim())
+                if(itemSearch){
+                    const CSPData = {
+                        name: itemSearch.name,
+                        categorySubcategoryId: itemSearch._id,
+                        type: 'item',
+                        profileProviderId:newProfile._id
+                    }
+
+                    const cSProfile: ICategorySubcategoryProfile = new CategorySubcategoryProfile(CSPData)
+                    await cSProfile.save()
+                }
             })
+
+            subcategoriesCompanyData.forEach(async (s:any)=>{
+                const subitemSearch = subitems.find(subitem=>subitem.name.toLowerCase().trim() === s.subcategoryName.toLowerCase().trim())
+                if(subitemSearch){
+                    const CSPData = {
+                        name: subitemSearch.name,
+                        categorySubcategoryId: subitemSearch._id,
+                        type: 'subitem',
+                        profileProviderId:newProfile._id
+                    }
+                    const cSProfile: ICategorySubcategoryProfile = new CategorySubcategoryProfile(CSPData)
+                    await cSProfile.save()
+                }
+            })     
+            
+            //SAVE PRODUCTS
+
+            console.log('resp.companyid', resp.companyid)
+
+            const query4 = `
+                SELECT 
+                id,
+                name,
+                description,
+                featured,
+                promotionalprice,
+                price
+                FROM services
+                WHERE companyid = ${resp.companyid} `
+            const servicesData:any = (await execute(query4))
+
+            servicesData.forEach(async(s:any) => {
+
+                //QUERY IMAGES
+                const query5 = `
+                    SELECT *
+                    FROM product_images
+                    WHERE productid = ${s.id} `
+                const productImagesData:any = (await execute(query5))
+
+                let images = []
+
+                if(productImagesData){                    
+                    images = productImagesData.map((p:any)=>{return p.image})
+                }
+
+                const product = {
+                    name: s.name,
+                    profileProviderId: newProfile._id,
+                    description: s.description,
+                    images: images,
+                    price: parseInt(s.price) || null,
+                    isFeatured: s.featured,
+                    promotionalPrice: parseInt(s.promocionalprice) || null,
+                    categoryId: '',
+                }
+                const prod: IProduct = new Product(product)
+                await prod.save()                
+            });
+
+            
+            
+            return res.status(200).json({
+                message: `Migración completada`,
+                data:newUrl
+            })
+
+            
 
 
         }else{
